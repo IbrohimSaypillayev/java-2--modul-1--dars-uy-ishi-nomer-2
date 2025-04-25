1e9c2ae16a54dca7adb59eef2417344cad9905aa
IbrohimSaypillayev <sarahconoruzb@gmail.com>
2025-04-25 16:35:31 +0500

    jwiugwirjrfjiueigjff
    ofijrigjuerifjreugv

---
+++ scrip.js
@@ -0,0 +1,9 @@
+function myFunction1() {
+  const element = document.querySelector("body");
+  element.style.backgroundColor = '#f89d27'
+}
+
+function myDarkMode() {
+  let element1 = document.querySelector("body");
+  element1.style.backgroundColor = 'black'
+}
